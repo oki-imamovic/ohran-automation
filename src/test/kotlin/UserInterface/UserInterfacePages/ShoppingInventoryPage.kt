53fdac8dabc80c0d96e0199fd54bb8bc5fc2831e
Ohran <oki.imamovic@gmail.com>
2023-11-25 15:27:51 +0100

    add filter trough page with error handling for Stale Element Reference Exception

--- src/test/kotlin/UserInterface/UserInterfacePages/ShoppingInventoryPage.kt
+++ src/test/kotlin/UserInterface/UserInterfacePages/ShoppingInventoryPage.kt
@@ -1,6 +1,7 @@
 
 package UserInterface.UserInterfacePages // ktlint-disable package-name
 
+import org.openqa.selenium.StaleElementReferenceException
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
@@ -15,5 +16,43 @@ class ShoppingInventoryPage(driver: WebDriver) {
     @FindBy(className = "inventory_list")
     var inventoryListContainer: WebElement? = null
 
+    @FindBy(className = "product_sort_container")
+    var sortDropMenu: WebElement? = null
+
+    @FindBy(className = "inventory_item_name")
+    var inventoryItemsList: MutableList<WebElement>? = null
+
+    @FindBy(xpath = "//select[@class='product_sort_container']//option")
+    var sortOptions: MutableList<WebElement>? = null
+
     val isInventoryListContainerDisplayed: Boolean get() = inventoryListContainer!!.isDisplayed
+
+    fun hasListOrderChanged(itemName: String): Boolean {
+        if (inventoryItemsList!![0].text == itemName) {
+            return true
+        }
+        return false
+    }
+
+    fun sortItems(sortOption: String) {
+        var attempts = 0
+        while (attempts < MAX_ATTEMPTS) {
+            try {
+                sortDropMenu!!.click()
+                for (option in sortOptions!!) {
+                    if (option.text == sortOption) {
+                        option.click()
+                        return
+                    }
+                }
+            } catch (e: StaleElementReferenceException) {
+                attempts++
+            }
+        }
+        println("Element not found after $MAX_ATTEMPTS attempts.")
+    }
+
+    companion object {
+        const val MAX_ATTEMPTS = 3
+    }
 }
--- src/test/kotlin/UserInterface/UserInterfaceTests/ShoppingTest.kt
+++ src/test/kotlin/UserInterface/UserInterfaceTests/ShoppingTest.kt
@@ -4,6 +4,7 @@ package UserInterface.UserInterfaceTests // ktlint-disable package-name
 import UserInterface.TestBase
 import UserInterface.UserInterfacePages.ShoppingInventoryPage
 import UserInterface.UserInterfacePages.ShoppingLoginPage
+import org.testng.Assert.assertFalse
 import org.testng.Assert.assertTrue
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
@@ -33,6 +34,14 @@ class ShoppingTest : TestBase() {
         // right login
         shoppingLoginPage!!.doLogin(USER_NAME, PASSWORD)
         assertTrue(shoppingInventoryPage!!.isInventoryListContainerDisplayed)
+        shoppingInventoryPage!!.sortItems(SORT_Z_TO_A)
+        assertFalse(shoppingInventoryPage!!.hasListOrderChanged(BACKPACK))
+        shoppingInventoryPage!!.sortItems(SORT_PRICE_LOW_TO_HIGH)
+        assertFalse(shoppingInventoryPage!!.hasListOrderChanged(BACKPACK))
+        shoppingInventoryPage!!.sortItems(SORT_PRICE_HIGH_TO_LOW)
+        assertFalse(shoppingInventoryPage!!.hasListOrderChanged(BACKPACK))
+        shoppingInventoryPage!!.sortItems(SORT_A_TO_Z)
+        assertTrue(shoppingInventoryPage!!.hasListOrderChanged(BACKPACK))
     }
 
     companion object {
@@ -40,5 +49,10 @@ class ShoppingTest : TestBase() {
         const val WRONG_PASSWORD: String = "wrong_password"
         const val USER_NAME: String = "standard_user"
         const val PASSWORD: String = "secret_sauce"
+        const val BACKPACK: String = "Sauce Labs Backpack"
+        const val SORT_A_TO_Z: String = "Name (A to Z)"
+        const val SORT_Z_TO_A: String = "Name (Z to A)"
+        const val SORT_PRICE_LOW_TO_HIGH: String = "Price (low to high)"
+        const val SORT_PRICE_HIGH_TO_LOW: String = "Price (high to low)"
     }
 }
